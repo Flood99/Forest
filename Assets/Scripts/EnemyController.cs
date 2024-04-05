ec72478807327d414d1df7f09af3620e01ba6039
Flood99 <113184223+Flood99@users.noreply.github.com>
2024-04-05 12:36:40 -0400

    4

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Ai;
+
+public class EnemyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject player;
+    private NavMeshAgent navMesh;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
