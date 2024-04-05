6696a4cc19d2085b4706af5ffa2b149e33924e9b
Flood99 <113184223+Flood99@users.noreply.github.com>
2024-04-05 13:20:06 -0400

    enemy

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,21 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Ai;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
     // Start is called before the first frame update
     private GameObject player;
-    private NavMeshAgent navMesh;
+    private NavMeshAgent enemy;
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player");
+        enemy = GetComponent<NavMeshAgent>(); 
+        enemy.SetDestination(player.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemy.SetDestination(player.transform.position);
     }
 }
