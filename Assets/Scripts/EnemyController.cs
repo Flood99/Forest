72a25014d214deff4a592aeb31b9b5882472b651
Flood99 <113184223+Flood99@users.noreply.github.com>
2024-04-08 12:47:26 -0400

    tree moves

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,9 +5,12 @@ using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
+
     // Start is called before the first frame update
+    private float maxSpeed = 10;
     private GameObject player;
     private NavMeshAgent enemy;
+    private string STATE = "CHASE";
     void Start()
     {
         player = GameObject.FindWithTag("Player");
@@ -17,7 +20,40 @@ public class EnemyController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    
     {
-        enemy.SetDestination(player.transform.position);
+        // get the direction player is facing 
+        Vector3 targetDir =  transform.position - player.transform.position;
+
+        // returns the angle (from, to)
+        float angleToUs= Vector3.Angle(targetDir, player.transform.forward);
+
+        if(STATE == "IDLE")
+        {
+            if (angleToUs >= -70 && angleToUs<= 70)
+            {
+                 
+            }else{
+                STATE = "CHASE";
+            }
+            enemy.speed = 0;
+
+        }else if(STATE == "STALK"){
+
+            enemy.speed = 3;
+        }else if(STATE == "CHASE"){
+            enemy.SetDestination(player.transform.position);
+            enemy.speed = 15;
+           
+            // when the angle is at -90 or +90, then it is in view (180º FOV)
+            if (angleToUs >= -70 && angleToUs<= 70)
+            {
+                STATE = "IDLE";
+            }
+        }else if(STATE == "RUN"){
+
+            
+        }
+       
     }
 }
